901423d5c0ab602fdd514831c65cd66cfce16dba
kangarooswapfinance <kangarooswapfinance@gmail.com>
2023-03-18 02:33:03 +0700

    Update index.tsx

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -60,13 +60,13 @@ export const PageTabs = () => {
     <>
       <MenuItem href="/swap" isActive={pathname.startsWith('/swap')}>
         <Trans>Swap</Trans>
-      </MenuItem>
+      </MenuItem>{/*
       <MenuItem href={`/tokens/${chainName.toLowerCase()}`} isActive={pathname.startsWith('/tokens')}>
         <Trans>Tokens</Trans>
       </MenuItem>
       <MenuItem dataTestId="nft-nav" href="/nfts" isActive={isNftPage}>
         <Trans>NFTs</Trans>
-      </MenuItem>
+      </MenuItem>*/}
       <MenuItem href="/pools" dataTestId="pool-nav-link" isActive={isPoolActive}>
         <Trans>Pools</Trans>
       </MenuItem>
